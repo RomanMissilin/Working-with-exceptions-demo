7dd9f790b38ef7f266ebc94b95c7ba744ced8f2e
Sam Smith <romanmissilin@mail.ru>
2022-05-15 13:52:28 +0300

    Начало

---
+++ Working-with-exceptions-demo/src/main/java/Working/with/exceptions/Workingwithexceptionsdemo/WorkingWithExceptionsDemoApplication.java
@@ -0,0 +1,13 @@
+package Working.with.exceptions.Workingwithexceptionsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingWithExceptionsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingWithExceptionsDemoApplication.class, args);
+	}
+
+}
